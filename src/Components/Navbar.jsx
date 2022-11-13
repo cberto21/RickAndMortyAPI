159e9ba96d715a2e46f32fb796db5672056f86f7
cberto21 <camila.bertolani@estudiantes.unahur.edu.ar>
2022-11-13 14:24:51 -0300

    build

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -4,12 +4,17 @@ import './style/logo.css'
 const Navbar = () => {
 
   return (
-    <div>
+    <div className='wrapper'>
       <nav className="navbar navbar-dark bg-dark">
         <div className="container">
+        <div className='justify-align-content-md-start' >
           <a href="/">
             <img src={logo} className="logoSize" />
           </a>
+          </div>
+          <div className='justify-content-end' >
+
+
           <a
             className="text-uppercase navbar-brand typeFace"
             target="_blank"
@@ -17,6 +22,7 @@ const Navbar = () => {
           >
             Ver capitulos online
           </a>
+          </div>
         </div>
       </nav>
     </div>
